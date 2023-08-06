835fe7e25eb8ccb97625f4ba7ede754da4846def
Jason Liu <111265664+liug26@users.noreply.github.com>
2023-08-06 17:13:38 +0800

    Added --output argument

--- data-analysis/log2plot.py
+++ data-analysis/log2plot.py
@@ -4,18 +4,19 @@ import plotly.graph_objects as go
 import sys
 
 
-# Usage example: python log2plot.py -i /Users/jasonliu/Downloads/0037.csv -p accx
+# Usage example: python log2plot.py -i Downloads/0037.csv -p accx
 print('Parsing arguments:', sys.argv)
 parser = argparse.ArgumentParser()
-parser.add_argument('-p', '--plot', nargs='+', required=True, help="")
-parser.add_argument('-i', '--input', required=True, help="")
-parser.add_argument('-t', '--title', required=False, default='Plot Title', help="")
-parser.add_argument('-x', '--xaxis_title', required=False, default='Time (s)', help="")
-parser.add_argument('-y', '--yaxis_title', required=False, default='Y Axis Title', help="")
-parser.add_argument('-l', '--legend_title', required=False, default='Legend', help="")
-parser.add_argument('--font_family', required=False, default='Courier New, monospace', help="")
-parser.add_argument('--font_size', required=False, type=int, default=18, help="")
-parser.add_argument('--font_color', required=False, default='RebeccaPurple', help="")
+parser.add_argument('-p', '--plot', nargs='+', required=True, help="space-separated data types")
+parser.add_argument('-i', '--input', required=True, help="input csv log file")
+parser.add_argument('-o', '--output', required=True, default='tempplot.html', help="output html file")
+parser.add_argument('-t', '--title', required=False, default='Plot Title', help="graph title")
+parser.add_argument('-x', '--xaxis_title', required=False, default='Time (s)', help="x axis title")
+parser.add_argument('-y', '--yaxis_title', required=False, default='Y Axis Title', help="y axis title")
+parser.add_argument('-l', '--legend_title', required=False, default='Legend', help="legend title")
+parser.add_argument('--font_family', required=False, default='Courier New, monospace', help="font family")
+parser.add_argument('--font_size', required=False, type=int, default=18, help="font size")
+parser.add_argument('--font_color', required=False, default='RebeccaPurple', help="font color")
 
 args = parser.parse_args()
 
@@ -44,16 +45,17 @@ def main():
     for plot_data in args.plot:
         # check if plot_data is a valid key of data_parser.data
         if plot_data not in data_parser.data.keys():
-            print('Warning:', plot_data, 'is not a valid data key, skipping')
+            print('Warning:', plot_data, 'is not a valid data type, skipping')
             continue
         print('Adding', plot_data, 'data to figure')
         data = data_parser.data[plot_data]
         mode = 'markers' if data.is_bool else 'markers+lines'
         fig.add_trace(go.Scatter(x=data.x, y=data.y, mode=mode, name=data.title))
+
     print('Showing figure')
     fig.show()
     print('Writing figure to html')
-    fig.write_html('tempplot.html')
+    fig.write_html(args.output)
 
 
 if __name__ == '__main__':
